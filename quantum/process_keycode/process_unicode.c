1cae14b87f21f52722401ef6d581de99674bd6d0
Edward Shen <code@eddie.sh>
2019-01-30 18:00:39 -0500

    rebased with upstream

--- quantum/process_keycode/process_unicode.c
+++ quantum/process_keycode/process_unicode.c
@@ -19,7 +19,12 @@
 
 bool process_unicode(uint16_t keycode, keyrecord_t *record) {
   if (keycode > QK_UNICODE && record->event.pressed) {
-    uint16_t unicode = keycode & 0x7FFF;
+    if (first_flag == 0) {
+      set_unicode_input_mode(eeprom_read_byte(EECONFIG_UNICODEMODE));
+      first_flag = 1;
+    }
+
+    uint16_t unicode = (keycode >= 0xFF00) ? keycode : keycode & 0x7FFF;
     unicode_input_start();
     register_hex(unicode);
     unicode_input_finish();
